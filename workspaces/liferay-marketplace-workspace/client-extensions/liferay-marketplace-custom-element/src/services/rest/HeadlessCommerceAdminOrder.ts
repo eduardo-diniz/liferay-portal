435e7641a1a70602efb29b3b8772c208135f98d2
Eduardo Diniz <eduardo.diniz@liferay.com>
2025-06-09 15:21:41 -0300

    LPD-56488 implement dynamic GraphQL query for orders

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/services/rest/HeadlessCommerceAdminOrder.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/services/rest/HeadlessCommerceAdminOrder.ts
@@ -5,6 +5,13 @@
 
 import fetcher from '../fetcher';
 
+type Metrics = {
+	[key: string]: {
+		items: Order[];
+		totalCount: number;
+	};
+};
+
 export default class HeadlessCommerceAdminOrder {
 	static deleteOrder(orderId: number | string) {
 		return fetcher.delete(
@@ -16,5 +23,49 @@ export default class HeadlessCommerceAdminOrder {
 		return fetcher<APIResponse>(
 			`o/headless-commerce-admin-order/v1.0/orders?${searchParams.toString()}`
 		);
+	}
+
+	static async getOrdersDashboardMetrics(filters: Record<string, string>) {
+		const orderQueries = Object.entries(filters)
+			.map(([alias, filter]) => {
+				return `${alias}: orders(filter: "${filter}", pageSize: 1) {
+					items {
+						orderStatus
+						totalAmount
+				 	}
+					totalCount
+				}`;
+			})
+			.join('\n');
+
+		const query = `
+		{
+			metrics: headlessCommerceAdminOrder_v1_0 {
+				${orderQueries}
+			}
+		}
+		`;
+
+		try {
+			const response = await fetcher.post<{
+				data: {
+					metrics: Metrics;
+				};
+			}>(`/o/graphql`, {query});
+
+			return response;
+		}
+		catch {
+			const metrics: Metrics = {};
+			for (const key in filters) {
+				metrics[key] = {items: [], totalCount: 0};
+			}
+
+			return {
+				data: {
+					metrics,
+				},
+			};
+		}
 	}
 }
