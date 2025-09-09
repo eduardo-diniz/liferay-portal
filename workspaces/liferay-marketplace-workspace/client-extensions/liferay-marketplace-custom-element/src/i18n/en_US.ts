b5dcc38a72785958d50474ff7177ec4ed9c3da27
Eduardo Diniz <eduardo.diniz@liferay.com>
2025-09-09 18:42:34 -0300

    LPD-64450 Update next step page

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/i18n/en_US.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/i18n/en_US.ts
@@ -146,6 +146,7 @@ export default {
 	'content-media-type': 'Content Media Type',
 	'content-policy': 'Content policy',
 	'continue': 'Continue',
+	'continue-to-download': 'Continue to Download',
 	'continue-to-install': 'Continue to Install',
 	'copied-x-to-the-clipboard': 'Copied {0} to the clipboard',
 	'could-not-upload-the-file-package-with-this-filename-already-exists':
@@ -279,6 +280,7 @@ export default {
 	'go-to-app-provisioning': 'Go to App Provisioning',
 	'go-to-cloud-console': 'Go to Cloud Console',
 	'go-to-dashboard': 'Go to Dashboard',
+	'go-to-download': 'Go to Download',
 	'go-to-dxp': 'Go to DXP',
 	'go-to-marketplace': 'Go to Marketplace',
 	'go-to-my-apps': 'Go to My Apps',
