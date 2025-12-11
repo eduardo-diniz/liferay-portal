8a332e842831fb77cd2c1b4c06c9d6d041226b5a
Eduardo Diniz <eduardo.diniz@liferay.com>
2025-12-11 12:05:37 -0300

    LPD-64746 Simplify

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
@@ -492,8 +492,8 @@ public class MarketplaceRestController extends BaseRestController {
 			});
 	}
 
-	@PostMapping("/process-publisher-assets/{productId}")
-	public void processPublisherAssets(@PathVariable long productId) {
+	@PostMapping("/process-publisher-asset-link/{productId}")
+	public void processPublisherAssetLink(@PathVariable long productId) {
 		try {
 			Product product = _marketplaceService.getProduct(productId);
 
@@ -525,7 +525,7 @@ public class MarketplaceRestController extends BaseRestController {
 			}
 		}
 		catch (Exception exception) {
-			String errorMessage = String.format(
+			String errorMessage = StringBundler.concat(
 				"Failed to upload publisher asset for product ID %d: %s",
 				productId, exception.getMessage());
 
@@ -576,12 +576,12 @@ public class MarketplaceRestController extends BaseRestController {
 	private File _getPublisherAssetFile(String publisherAssetURL)
 		throws Exception {
 
-		InputStream inputStream =
-			_marketplaceService.getPublisherAssetInputStream(publisherAssetURL);
-
 		File tempFile = File.createTempFile("publisher_asset_", ".zip");
 
-		try (FileOutputStream fileOutputStream = new FileOutputStream(
+		try (InputStream inputStream =
+				_marketplaceService.getPublisherAssetInputStream(
+					publisherAssetURL);
+			FileOutputStream fileOutputStream = new FileOutputStream(
 				tempFile)) {
 
 			inputStream.transferTo(fileOutputStream);
@@ -634,8 +634,8 @@ public class MarketplaceRestController extends BaseRestController {
 			PublisherAssetLink publisherAssetLink)
 		throws Exception {
 
-		File publisherAssetFile = null;
 		File publisherAssetArtifactFile = null;
+		File publisherAssetFile = null;
 
 		try {
 			publisherAssetFile = _getPublisherAssetFile(
