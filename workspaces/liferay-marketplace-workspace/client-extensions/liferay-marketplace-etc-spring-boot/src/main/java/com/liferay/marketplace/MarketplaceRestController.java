602ef4191b72b3fb0e7ee256acadee7aace0143e
Brian Chan <brian.chan@liferay.com>
2025-12-23 23:59:48 +0530

    LPD-64746 SF

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
@@ -517,7 +517,7 @@ public class MarketplaceRestController extends BaseRestController {
 			if (publisherAssetLinks.isEmpty()) {
 				if (_log.isInfoEnabled()) {
 					_log.info(
-						"No publisher asset links to process product " +
+						"No publisher asset links to process for product " +
 							productId);
 				}
 
@@ -668,9 +668,8 @@ public class MarketplaceRestController extends BaseRestController {
 				publisherAssetLink.getAttachmentId());
 		}
 		finally {
-			MarketplaceUtil.deleteTempFile(publisherAssetFile, false);
-
 			MarketplaceUtil.deleteTempFile(publisherAssetArtifactFile, true);
+			MarketplaceUtil.deleteTempFile(publisherAssetFile, false);
 		}
 	}
 
